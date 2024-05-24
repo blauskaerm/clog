ad92b5f6e21f7ea9c2c535ed9bca49ec4c3a9700
BlauskaerM <blauskaerm@protonmail.ch>
2024-05-24 23:21:28 +0200

    Fix color bug

--- clog.c
+++ clog.c
@@ -11,7 +11,7 @@
 #define COLOR_YELLOW "\033[93m"
 #define COLOR_GREEN  "\033[92m"
 #define COLOR_BLUE   "\033[94m"
-#define COLOR_WHITE  "\033[93m"
+#define COLOR_WHITE  "\033[97m"
 #define COLOR_RED    "\033[91m"
 #define COLOR_RESET  "\033[0m"
 
